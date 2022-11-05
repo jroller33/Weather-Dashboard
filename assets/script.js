6b894e327cf8b9b3c6405a8caf4e2f1416354f72
John Roller <jtroller33@gmail.com>
2022-11-05 09:22:44 -0500

    getCurrentWeather and getForecast are working

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ function getSearches() {
       let srchBtn = $("<button>")
         .text(searchHistory[index])
         .addClass("btn1 button is-blue ml-1 has-text-centered mt-3");
-      $(".recentSearches").append(srchBtn);
+      $(".searchHistory").append(srchBtn);
     });
 
     // $(".btn1").click(function (event) {
@@ -32,16 +32,19 @@ function getSearches() {
   let newRowEl = $("<tr>");
 let weatherElement = $(".currentWeather");
   
-//Five day forecast
+
+
+
+
 function getForecast(lat, lon, weather, name, temp, wind, humidity) {
-  let apiCall =
+  let apiUrl =
   "https://api.openweathermap.org/data/2.5/onecall?lat=" +
   lat +
     "&lon=" +
     lon +
     "&exclude=alerts,hourly,minutely&appid=0830fec5fefb765b207129fdb7fcdf86&units=imperial";
     $.ajax({
-    url: apiCall,
+    url: apiUrl,
     method: "GET",
   }).then(function (data) {
     let date = dayjs().format("MM/DD/YYYY")
@@ -65,5 +68,38 @@ function getForecast(lat, lon, weather, name, temp, wind, humidity) {
 
     $("#weatherTable").empty();
     postForecast(data);
+  });
+}
+
+
+
+function getCurrentWeather(city) {
+
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=0830fec5fefb765b207129fdb7fcdf86&units=imperial";
+
+  $.ajax({
+
+    url: apiUrl,
+    method: "GET",
+  }).then(function (data) {
+
+    $("#currentCondition").empty()
+
+    // elements for forecast table
+    let city = data.name;
+    let temperature = data.main.temp + "℉"
+    let humidity = data.main.humidity + "%"
+    let wind = data.wind.speed + "MPH"
+
+    
+    // latitude and longitude for new fetch
+    let lat = data.coord.lat;
+    let long = data.coord.lon;
+
+    getForecast(lat, long, city, temperature, humidity, wind);
+    updateSearches(city);
   });
 }
